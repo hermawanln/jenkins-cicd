543bfb049540378224e76d82ae6a772fa6e12bbe
hermawanln <hermawan.luthfi@gmail.com>
2022-01-18 10:27:34 +0700

    add job-dsl

---
+++ job-dsl/README.md
@@ -0,0 +1,5 @@
+### To activate this feature, you need to install plugin "Job DSL" in Jenkins
+
+
+## Usable Link
+1. https://jenkinsci.github.io/job-dsl-plugin/#method/javaposse.jobdsl.dsl.helpers.step.StepContext.dockerBuildAndPublish
---
+++ job-dsl/dockerBuildPublish.groovy
@@ -0,0 +1,24 @@
+job('jenkins-cicd-deploy') {
+    scm {
+        git('https://github.com/hermawanln/jenkins-cicd.git') {  node ->
+            node / gitConfigName('hermawanln')
+            node / gitConfigEmail('hermawan.luthfi@gmail.com')
+        }
+    }
+    triggers {
+        scm('* * * * *')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('hermawanln/jenkins-cicd')
+            tag('${BUILD_TIMESTAMP}-${GIT_REVISION,length=7}') //unique id dari git yang digunakan untuk auto generate SHA 
+            // registryCredentials('dockerhub')
+            // dockerHostURI('unix:///var/run/docker.sock') // Specifies the URI to the Docker host.
+            dockerRegistryURL('http://192.168.1.100:5000/') // Sets the URL of the Docker registry.
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
